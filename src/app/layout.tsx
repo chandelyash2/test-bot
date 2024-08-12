1ec3dd48cf924ad2d7b74e49811ca3261a85a358
Bitfire <chandel.yash2@gmail.com>
2024-08-12 23:45:03 +0530

    add sebouncesdada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body
         className={`${manrope.className} ${istok.className} ${inter.className} ${roboto.className} `}
       >
